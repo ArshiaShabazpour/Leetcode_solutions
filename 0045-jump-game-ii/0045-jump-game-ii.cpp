71636bf5abff6ad438b2b5db1ca07d646179ac5e
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-01-30 16:08:57 -0500

    Time: 0 ms (100%), Space: 20.3 MB (98.23%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -6,16 +6,19 @@ public:
     int jump(vector<int>& nums) {
         if (nums.size() == 1) return 0;
 
-        int end = 0;
-        int maxReach = 0;
-        int counter = 0;
+        int start = 0;  
+        int end = nums[0];  
+        int maxReach = 0;  
+        int counter = 1;  
 
         for (int i = 0; i < nums.size() - 1; i++) {
             maxReach = max(maxReach, i + nums[i]); 
             
             if (i == end) { 
+                start = end; 
                 counter++;
-                end = maxReach;
+                end = maxReach; 
+                
                 if (end >= nums.size() - 1) break; 
             }
         }
