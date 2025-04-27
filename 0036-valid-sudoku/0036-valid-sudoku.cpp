aecf340c280d7a22992d9e6626582afff65edfa7
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-04-26 20:55:11 -0400

    Time: 0 ms (100%), Space: 21.9 MB (82.04%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int rows[9]  = {0};
-        int cols[9]  = {0};
-        int boxes[9] = {0};
+        bool rows[9][9]   = {false};
+        bool cols[9][9]   = {false};
+        bool boxes[9][9]  = {false};
 
         for (int r = 0; r < 9; ++r) {
             for (int c = 0; c < 9; ++c) {
                 char ch = board[r][c];
                 if (ch == '.') continue;
 
-                int d = ch - '1';             
-                int mask = 1 << d;             
-                int b = (r/3)*3 + c/3;          
+                int d = ch - '1';                 
+                int b = (r / 3) * 3 + (c / 3);    
 
-                if ((rows[r]  & mask) ||
-                    (cols[c]  & mask) ||
-                    (boxes[b] & mask)) {
+                if (rows[r][d] || cols[c][d] || boxes[b][d]) {
                     return false;
                 }
 
-                rows[r]  |= mask;
-                cols[c]  |= mask;
-                boxes[b] |= mask;
+                rows[r][d]   = true;
+                cols[c][d]   = true;
+                boxes[b][d]  = true;
             }
         }
         return true;
