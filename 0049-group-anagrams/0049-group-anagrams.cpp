9e03cffbdc357ced15d7d5bec1134e6cb426a110
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-04-29 17:45:33 -0400

    Time: 11 ms (91.98%), Space: 24.9 MB (76.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -4,20 +4,14 @@ public:
         unordered_map<string, vector<string>> ans;
 
         for (string& s : strs) {
-            array<int, 26> count = {0};
-            for (char c : s) {
-                count[c - 'a']++;
-            }
-            string key;
-            for (int num : count) {
-                key += to_string(num) + "#";
-            }
+            string key = s;
+            sort(key.begin(), key.end());
             ans[key].push_back(s);
         }
 
         vector<vector<string>> result;
         for (auto& entry : ans) {
-            result.push_back(move(entry.second));
+            result.push_back(entry.second);
         }
 
         return result;        
