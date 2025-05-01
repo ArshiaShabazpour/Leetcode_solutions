274ca260e4eb596a5483e5bbd70e026c9f31e013
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-04-30 20:30:32 -0400

    Time: 44 ms (98.37%), Space: 74.1 MB (91.69%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -2,7 +2,14 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_set<int> seen;
-
+        if (nums.size()>7){
+            sort(nums.begin() ,nums.end());
+            for (int j=0;j<nums.size()-1;j++){
+                if (nums[j]==nums[j+1])
+                    return true;
+            }
+        }
+        else{
         for (int i = 0; i < nums.size(); i++) {
             if (i > k) {
                 seen.erase(nums[i - k - 1]);
@@ -13,6 +20,7 @@ public:
             }
 
             seen.insert(nums[i]);
+        }
         }
 
         return false;        
