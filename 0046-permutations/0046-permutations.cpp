b7a4698c042d46afab917ffc7a3ead49639e0d0c
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-05-07 20:41:42 -0400

    Time: 1 ms (44.49%), Space: 10.6 MB (69.81%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> perms;
-        vector<int> perm;
-        backtrack(nums, perm, perms);
-        return perms;
+    void backtrack(const vector<int>& nums, 
+                   vector<int>& rest_nums, vector<int>& path, vector<vector<int>>& permutations){
+        for(int i=0; i<rest_nums.size(); ++i){
+            int val = rest_nums[i];
+            rest_nums.erase(rest_nums.begin() + i);
+            path.push_back(val);
+            if(path.size() == nums.size())
+            {
+                permutations.push_back(path);
+            }
+            else
+            {
+                backtrack(nums, rest_nums, path, permutations);
+            }
+            path.pop_back();
+            rest_nums.insert(rest_nums.begin() + i, val);
+        }
     }
 
-private:
-    void backtrack(vector<int> nums, vector<int>& perm, vector<vector<int>>& perms) {
-        if (nums.empty()) {
-            perms.push_back(perm);
-            return;
-        }
-        for (int i = 0; i < nums.size(); ++i) {
-            vector<int> next_nums = nums;
-            perm.push_back(nums[i]);
-            next_nums.erase(next_nums.begin() + i);
-            backtrack(next_nums, perm, perms);
-            perm.pop_back();
-        }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> permutations;
+
+        vector<int> rest_nums = nums;
+        vector<int> path;
+        backtrack(nums, rest_nums, path, permutations);
+
+        return permutations;
     }
 };
