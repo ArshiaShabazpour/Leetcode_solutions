d140cee39863bf19d8e6dd6ffee198815248e017
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-05-09 12:11:50 -0400

    Time: 1350 ms (10.13%), Space: 113.2 MB (8.78%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -4,10 +4,12 @@ public:
         int rows = board.size();
         int cols = board[0].size();
         unordered_set<string> visited;
+
         auto dfs = [&](int r, int c, int k, auto& dfs) -> bool {
             if (k == word.length()) {
                 return true;
             }
+
             if (r < 0 || r >= rows || c < 0 || c >= cols || visited.count(to_string(r) + "," + to_string(c)) || board[r][c] != word[k]) {
                 return false;
             }
@@ -17,13 +19,16 @@ public:
             visited.erase(to_string(r) + "," + to_string(c));
             return res;
         };
+
         unordered_map<char, int> count;
         for (char c : word) {
             count[c]++;
         }
+
         if (count[word[0]] > count[word.back()]) {
             reverse(word.begin(), word.end());
         }
+
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
                 if (dfs(r, c, 0, dfs)) {
@@ -31,6 +36,7 @@ public:
                 }
             }
         }
+
         return false;        
     }
 };
