d30e7b84d014a37f5944782dcb5ab35e066df288
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-06-23 13:31:28 -0400

    Time: 0 ms (100%), Space: 19 MB (45.32%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,10 +11,10 @@
  */
 class Solution {
 public:
-int maxDepth(TreeNode* root) {
-        if(!root) return 0;
-        int maxLeft = maxDepth(root->left);
-        int maxRight = maxDepth(root->right);
-        return max(maxLeft, maxRight)+1;
-    }
+    int maxDepth(TreeNode* root) {
+            if(!root) return 0;
+            int maxLeft = maxDepth(root->left);
+            int maxRight = maxDepth(root->right);
+            return max(maxLeft, maxRight)+1;
+        }
 };
