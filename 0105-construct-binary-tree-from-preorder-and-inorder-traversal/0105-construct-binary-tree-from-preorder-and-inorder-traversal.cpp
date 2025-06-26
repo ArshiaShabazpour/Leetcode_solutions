c584f3fec1da159c8b13fc1babe5d1c205d4c081
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-06-26 16:55:38 -0400

    Time: 4 ms (58.25%), Space: 27.6 MB (36.76%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,19 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int rootIdx = 0;
-        return build(preorder, inorder, rootIdx, 0, inorder.size()-1);
+        unordered_map <int, int> inMap;
+        for(int i=0; i< inorder.size(); i++){
+            inMap[inorder[i]] = i;
+        }
+        TreeNode* root = Helper(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, inMap );
+        return root;
     }
-    
-    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int& rootIdx, int left, int right) {
-        if (left > right) return NULL;
-        int pivot = left;  // find the root from inorder
-        while(inorder[pivot] != preorder[rootIdx]) pivot++;
-        
-        rootIdx++;
-        TreeNode* newNode = new TreeNode(inorder[pivot]);
-        newNode->left = build(preorder, inorder, rootIdx, left, pivot-1);
-        newNode->right = build(preorder, inorder, rootIdx, pivot+1, right);
-        return newNode;
+    TreeNode* Helper(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd,unordered_map<int,int>& inMap){
+        if (preStart > preEnd || inStart > inEnd) return nullptr;
+
+        TreeNode* root = new TreeNode(preorder[preStart]);
+        int inRoot = inMap[root->val];
+        int numsLeft = inRoot - inStart;
+
+        root->left = Helper(preorder, preStart+1, preEnd + numsLeft, inorder, inStart , inRoot -1, inMap);
+        root->right= Helper(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot +1, inEnd, inMap);
+
+        return root;
+
     }
 };
