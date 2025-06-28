288005bb5afbf71ed4d6a797fe1814592c751910
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-06-28 19:59:56 -0400

    Time: 3 ms (70.69%), Space: 27 MB (92.37%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,37 +1,15 @@
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        return buildSubtree(inorder, 0, inorder.size() - 1,
-                            postorder, 0, postorder.size() - 1);
+    TreeNode *Tree(vector<int>& in, int x, int y,vector<int>& po,int a,int b){
+        if(x > y || a > b)return nullptr;
+        TreeNode *node = new TreeNode(po[b]);
+        int SI = x;  
+        while(node->val != in[SI])SI++;
+        node->left  = Tree(in,x,SI-1,po,a,a+SI-x-1);
+        node->right = Tree(in,SI+1,y,po,a+SI-x,b-1);
+        return node;
     }
-
-private:
-    TreeNode* buildSubtree(const vector<int>& inorder, int inL, int inR,
-                           const vector<int>& postorder, int postL, int postR) {
-        // base case
-        if (inL > inR || postL > postR) return nullptr;
-
-        // root value is at end of current postorder segment
-        int rootVal = postorder[postR];
-        TreeNode* root = new TreeNode(rootVal);
-
-        // find root index in inorder to split left/right
-        int i = inL;
-        while (i <= inR && inorder[i] != rootVal) ++i;
-        int leftSize = i - inL;  // number of nodes in left subtree
-
-        // build left subtree: inorder[inL..i-1], postorder[postL..postL+leftSize-1]
-        root->left = buildSubtree(
-            inorder, inL,            i - 1,
-            postorder, postL,        postL + leftSize - 1
-        );
-
-        // build right subtree: inorder[i+1..inR], postorder[postL+leftSize..postR-1]
-        root->right = buildSubtree(
-            inorder, i + 1,          inR,
-            postorder, postL + leftSize, postR - 1
-        );
-
-        return root;
+    TreeNode* buildTree(vector<int>& in, vector<int>& po){
+        return Tree(in,0,in.size()-1,po,0,po.size()-1);
     }
 };
