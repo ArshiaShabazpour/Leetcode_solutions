90596b9183b175b7fe86653673c3252d8e8f70f7
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-06-28 20:01:35 -0400

    Time: 3 ms (70.69%), Space: 27.5 MB (60.32%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,15 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+    unordered_map<int,int> umap;//val->ind
 public:
-    TreeNode *Tree(vector<int>& in, int x, int y,vector<int>& po,int a,int b){
-        if(x > y || a > b)return nullptr;
-        TreeNode *node = new TreeNode(po[b]);
-        int SI = x;  
-        while(node->val != in[SI])SI++;
-        node->left  = Tree(in,x,SI-1,po,a,a+SI-x-1);
-        node->right = Tree(in,SI+1,y,po,a+SI-x,b-1);
-        return node;
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        for(int i=0;i<inorder.size();i++){
+            umap[inorder[i]]=i;
+        }
+        int ind=postorder.size()-1;
+        return build(postorder, ind, 0, inorder.size()-1);
     }
-    TreeNode* buildTree(vector<int>& in, vector<int>& po){
-        return Tree(in,0,in.size()-1,po,0,po.size()-1);
+
+    TreeNode* build(vector<int> &postorder, int& ind, int st, int end){
+        if(ind<0 || st>end){return NULL;}
+        else{
+            TreeNode *node = new TreeNode(postorder[ind]);
+            int mid = umap[postorder[ind]];
+            ind--;
+            node->right = build(postorder, ind, mid+1, end);
+            node->left = build(postorder, ind, st, mid-1);
+            
+            return node;
+        }
     }
 };
