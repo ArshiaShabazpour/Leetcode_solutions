cbe3e0cf0549132c0689dcd1a4ea2d4fcf9686a3
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-06-30 15:44:05 -0400

    Time: 8 ms (69.01%), Space: 18.7 MB (92.46%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -1,21 +1,55 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
 class Solution {
 public:
-    Node* connect(Node *root) {
-        Node *now = root, *head = nullptr, *tail = nullptr;
-        while (now) {
-            if (now->left) {
-                if (tail) tail = tail->next = now->left;
-                else      head = tail = now->left;
+    Node* connect(Node* root) {
+        if (!root) return nullptr;
+
+        Node* curr = root;
+        Node* head = nullptr; // Head of the next level
+        Node* prev = nullptr; // The previous node on the next level
+
+        while (curr) {
+            while (curr) {
+                // Left child
+                if (curr->left) {
+                    if (prev) prev->next = curr->left;
+                    else head = curr->left;
+                    prev = curr->left;
+                }
+
+                // Right child
+                if (curr->right) {
+                    if (prev) prev->next = curr->right;
+                    else head = curr->right;
+                    prev = curr->right;
+                }
+
+                curr = curr->next; // Move in the current level
             }
-            if (now->right) {
-                if (tail) tail = tail->next = now->right;
-                else      head = tail = now->right;
-            }
-            if (!(now = now->next)) {
-                now = head;
-                head = tail = nullptr;
-            }
+
+            // Move to the next level
+            curr = head;
+            head = nullptr;
+            prev = nullptr;
         }
-        return root;            
+
+        return root;
     }
 };
