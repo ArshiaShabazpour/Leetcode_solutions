38a7df72993bfdeda7ca666e8fa16f3f0c21692d
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-07-16 20:19:33 -0400

    Time: 25 ms (72.87%), Space: 16.2 MB (79.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
-    void DFS(vector<vector<char>>& grid, int i, int j) {
-        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size())
-            return;
-        if(grid[i][j] == '2' || grid[i][j] == '0')
-            return;
-        
-        grid[i][j] = '2';
-        
-        DFS(grid, i+1, j);
-        DFS(grid, i, j-1);
-        DFS(grid, i-1, j);
-        DFS(grid, i, j+1);
-    }
-    
+    vector<pair<int, int>> movs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
     int numIslands(vector<vector<char>>& grid) {
- 
         int islands = 0;
-        for(int i = 0; i < grid.size(); i++) {
-            for(int j = 0; j < grid[0].size(); j++) {
-                if(grid[i][j] == '1') {
-                    DFS(grid, i, j);
-                    ++islands;
-                } 
+        int total_rows = grid.size();
+        int total_cols = grid[0].size();
+        for(int row=0; row<total_rows; row++){
+            for(int col=0; col<total_cols; col++){
+                if (grid[row][col] == '1'){
+                    dfs(grid, row, col, total_rows, total_cols);
+                    islands++;
+                }
             }
         }
         return islands;
     }
+
+private:
+
+    void dfs(vector<vector<char>>& grid, int row, int col, int total_rows, int total_cols){
+        if (grid[row][col] != '1'){
+            return;
+        }
+        grid[row][col] = '0';
+        for(pair<int, int> mov:movs){
+            int next_row = row + mov.first;
+            int next_col = col + mov.second;
+            if (is_inside_bounds(next_row, next_col, total_rows, total_cols)) {
+                dfs(grid, next_row, next_col, total_rows, total_cols);
+            }
+        }
+
+    }
+
+    bool is_inside_bounds(int row, int col, int total_rows, int total_cols){
+        return (row>=0 && row<total_rows && col>=0 && col<total_cols);
+    }
+    
 };
