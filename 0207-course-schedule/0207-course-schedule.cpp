b0bfd6eba38b8f4b9a2f0aa1c05d7e62ababa4f0
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-07-18 18:13:34 -0400

    Time: 0 ms (100%), Space: 19.3 MB (63.85%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,37 +1,32 @@
-#include <vector>
-#include <unordered_map>
-#include <functional>
-using namespace std;
-
 class Solution {
 public:
+    bool dfs(int node,vector<int> &vis,vector<vector<int>>& adj,vector<int> &pathVis){
+        vis[node]=1;
+        pathVis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                if(dfs(it,vis,adj,pathVis)==false) return false;
+            }
+            else if(pathVis[it]) return false;
+        }
+        pathVis[node]=0;
+        return true;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> pre;
-        for (auto &e : prerequisites)
-            pre[e[0]].push_back(e[1]);
-
-        vector<int> state(numCourses, 0); // 0 unvisited, 1 visiting, 2 done
-
-        function<bool(int)> dfs = [&](int course) -> bool {
-            if (!pre.count(course) || pre[course].empty()) {
-                state[course] = 2;
-                return true;
+        int m=prerequisites.size();
+        vector<int> vis(numCourses);
+        vector<int> pathVis(numCourses);
+        vector<vector<int>> adj(numCourses);
+        for(int i=0;i<m;i++){
+            int u=prerequisites[i][0];
+            int v=prerequisites[i][1];
+            adj[v].push_back(u);
+        }
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){
+                if(dfs(i,vis,adj,pathVis)==false) return false;
             }
-            if (state[course] == 1) return false; 
-            if (state[course] == 2) return true;
-
-            state[course] = 1;
-            for (int p : pre[course]) {
-                if (!dfs(p)) return false;
-            }
-            pre[course].clear();
-            state[course] = 2;
-            return true;
-        };
-
-        for (int c = 0; c < numCourses; ++c)
-            if (!dfs(c)) return false;
-
+        }
         return true;
     }
 };
