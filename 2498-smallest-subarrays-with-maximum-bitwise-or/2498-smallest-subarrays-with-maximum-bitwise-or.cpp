165c241a2db14af48a4ad1b4e14f3b20e57beba2
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-07-29 11:07:35 -0400

    Time: 15 ms (93.1%), Space: 60.7 MB (71.12%) - LeetHub

--- 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.cpp
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     vector<int> smallestSubarrays(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> lastSeen(30, 0), res(n, 1);
-        for (int i = n - 1; i >= 0; --i) {
-            for (int bit = 0; bit < 30; ++bit) {
-                if ((nums[i] & (1 << bit)) > 0)
-                    lastSeen[bit] = i;
-                res[i] = max(res[i], lastSeen[bit] - i + 1);
+        const int n=nums.size();
+        vector<int> ans(n);
+        vector<int> last(30, -1); 
+
+        for (int i=n-1; i>= 0; i--) {
+            const unsigned x=nums[i];
+            int j=i;
+            bitset<30> X(x);
+            for (int b=0; b<30; b++) {
+                if (X[b]) 
+                    last[b]=i;
+                j=max(j, last[b]);
             }
+            ans[i]=j-i+1;
         }
-        return res;
+
+        return ans;
     }
 };
