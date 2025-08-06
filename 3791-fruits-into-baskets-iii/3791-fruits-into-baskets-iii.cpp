fd79da65774e2c722b96e77cb51c6abadbcc051f
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-08-06 16:42:09 -0400

    Time: 28 ms (98.83%), Space: 184.1 MB (75.44%) - LeetHub

--- 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.cpp
+++ 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.cpp
@@ -1,62 +1,38 @@
 class Solution {
-    int n;
-    vector<int> seg;
-    void Update(int p) { 
-        seg[p] = max(seg[p << 1], seg[p << 1 | 1]); 
-    }
-
-    void Build(int p, int l, int r, vector<int>& baskets) {
-        if (l == r) {
-            seg[p] = baskets[l];
-            return;
-        }
-        int mid = (l + r) >> 1;
-        Build(p << 1, l, mid, baskets);
-        Build(p << 1 | 1, mid + 1, r, baskets);
-        Update(p);
-    }
-    void Assign(int x, int v, int p, int l, int r) {
-        if (x < l || x > r) {
-            return;
-        }
-        if (l == r) {
-            seg[p] = v;
-            return;
-        }
-        int mid = (l + r) >> 1;
-        Assign(x, v, p << 1, l, mid);
-        Assign(x, v, p << 1 | 1, mid + 1, r);
-        Update(p);
-    }
-    int FirstLarger(int v, int p, int l, int r) {
-        if (seg[p] < v) {
-            return r + 1;
-        }
-        if (l == r) {
-            return r;
-        }
-        int mid = (l + r) >> 1;
-        int lf = FirstLarger(v, p << 1, l, mid);
-        if (lf <= mid) {
-            return lf;
-        }
-        return FirstLarger(v, p << 1 | 1, mid + 1, r);
-    }
-
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        n = fruits.size();
-        seg.assign(4 * n + 1, 0);
-        Build(1, 0, n - 1, baskets);
-        int res = 0;
-        for (const auto& x : fruits) {
-            int pos = FirstLarger(x, 1, 0, n - 1);
-            if (pos == n) {
-                res++;
-            } else {
-                Assign(pos, 0, 1, 0, n - 1);
+        int n = baskets.size();
+        int N = 1;
+        while (N <= n) N <<= 1;
+
+        vector<int> segTree(N << 1);
+        for (int i = 0; i < n; ++i)
+            segTree[N + i] = baskets[i];
+        
+        for (int i = N - 1; i > 0; --i)
+            segTree[i] = max(segTree[2 * i], segTree[2 * i + 1]);
+
+        int count = 0;
+        for (int fruit : fruits) {
+            int index = 1;
+            if (segTree[index] < fruit) {
+                count++;
+                continue;
+            }
+
+            while (index < N) {
+                if (segTree[2 * index] >= fruit)
+                    index = 2 * index;
+                else
+                    index = 2 * index + 1;
+            }
+
+            segTree[index] = -1;
+            while (index > 1) {
+                index >>= 1;
+                segTree[index] = max(segTree[2 * index], segTree[2 * index + 1]);
             }
         }
-        return res;
+        return count;
     }
 };
