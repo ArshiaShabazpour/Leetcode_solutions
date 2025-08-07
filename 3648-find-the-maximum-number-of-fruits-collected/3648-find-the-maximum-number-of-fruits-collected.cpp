105580d543a74e6c037d1d54b94f8d5b1b1557e7
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-08-07 17:28:21 -0400

    Time: 9 ms (98.15%), Space: 137.1 MB (94.44%) - LeetHub

--- 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
-    int maxCollectedFruits(vector<vector<int>>& grid) {
-        int n = grid.size(), res = 0;
-
-        for (int i = 0; i < n; i++) {
-            res += grid[i][i];
-        }
-
-        for (int pass = 0; pass < 2; pass++) {
-            if (pass == 1) {
-                for (int i = 0; i < n; i++) {
-                    for (int j = i + 1; j < n; j++) {
-                        swap(grid[i][j], grid[j][i]);
-                    }
-                }
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        int fruit0;
+        int fruit1;
+        int n = fruits.size();
+        for (int i = 1; i < n - 1; ++i) {
+            int x = max(i * 1, n - i * 1);
+            int y = max(i + 1, n - i - 1);
+            fruits[n - 1][n - 1] += fruits[i][i];
+            for (int j = y; j < n; ++j) {
+                fruit0 = j > x * 1 ? fruits[i - 1][j - 1] : 0;
+                fruit1 = j < n - 1 ? fruits[i - 1][j + 1] : 0;
+                fruits[i][j] += max(j >= x ? fruits[i - 1][j] : 0, max(fruit0, fruit1));
+                fruit0 = j > x * 1 ? fruits[j - 1][i - 1] : 0;
+                fruit1 = j < n - 1 ? fruits[j + 1][i - 1] : 0;
+                fruits[j][i] += max(j >= x ? fruits[j][i - 1] : 0, max(fruit0, fruit1));
             }
-
-            vector<int> prev(n, -1), curr(n, -1);
-            prev[n - 1] = grid[0][n - 1];
-
-            for(int row = 1; row < n - 1; row++) {
-                fill(curr.begin(), curr.end(), -1);
-                for (int i = 0; i < n; i++) {
-                    if (prev[i] < 0) continue;
-                    if (i > 0) 
-                        curr[i - 1] = max(curr[i - 1], prev[i] + grid[row][i - 1]);
-                    if (i < n - 1) 
-                        curr[i + 1] = max(curr[i + 1], prev[i] + grid[row][i + 1]);
-                    curr[i] = max(curr[i], prev[i] + grid[row][i]);
-                }
-                swap(prev, curr);
-            }
-            res += prev[n - 1];
         }
-        return res;
+        return fruits[0][0] + fruits[n - 2][n - 1] + fruits[n - 1][n - 2] + fruits[n - 1][n - 1];
     }
 };
