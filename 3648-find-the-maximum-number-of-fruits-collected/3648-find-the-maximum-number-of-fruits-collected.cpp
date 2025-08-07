02522bdb2988d1912274f29f6912d72d93fe606c
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-08-07 17:14:36 -0400

    Time: 51 ms (80.25%), Space: 137.8 MB (91.98%) - LeetHub

--- 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.cpp
@@ -2,25 +2,32 @@ class Solution {
 public:
     int maxCollectedFruits(vector<vector<int>>& grid) {
         int n = grid.size(), res = 0;
-        for (int i = 0; i < n; i++) res += grid[i][i];
 
-        auto get = [&](int r, int c, int pass) -> int {
-            return (pass == 0) ? grid[r][c] : grid[c][r];
-        };
+        for (int i = 0; i < n; i++) {
+            res += grid[i][i];
+        }
 
         for (int pass = 0; pass < 2; pass++) {
-            vector<int> prev(n, -1), curr(n, -1);
-            prev[n - 1] = get(0, n - 1, pass);
+            if (pass == 1) {
+                for (int i = 0; i < n; i++) {
+                    for (int j = i + 1; j < n; j++) {
+                        swap(grid[i][j], grid[j][i]);
+                    }
+                }
+            }
 
-            for (int row = 1; row < n - 1; row++) {
+            vector<int> prev(n, -1), curr(n, -1);
+            prev[n - 1] = grid[0][n - 1];
+
+            for(int row = 1; row < n - 1; row++) {
                 fill(curr.begin(), curr.end(), -1);
                 for (int i = 0; i < n; i++) {
                     if (prev[i] < 0) continue;
-                    if (i > 0)
-                        curr[i - 1] = max(curr[i - 1], prev[i] + get(row, i - 1, pass));
-                    if (i < n - 1)
-                        curr[i + 1] = max(curr[i + 1], prev[i] + get(row, i + 1, pass));
-                    curr[i] = max(curr[i], prev[i] + get(row, i, pass));
+                    if (i > 0) 
+                        curr[i - 1] = max(curr[i - 1], prev[i] + grid[row][i - 1]);
+                    if (i < n - 1) 
+                        curr[i + 1] = max(curr[i + 1], prev[i] + grid[row][i + 1]);
+                    curr[i] = max(curr[i], prev[i] + grid[row][i]);
                 }
                 swap(prev, curr);
             }
