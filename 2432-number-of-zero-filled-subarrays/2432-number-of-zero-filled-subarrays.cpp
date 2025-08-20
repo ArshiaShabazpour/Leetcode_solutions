8ea4938896210bf85ddc5e096f870c34c035a43d
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-08-19 20:16:18 -0400

    Time: 0 ms (100%), Space: 111.4 MB (11.02%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long total = 0; 
-        long long counter = 0; 
-
+        long long cnt = 0, streak = 0;
         for (int num : nums) {
             if (num == 0) {
-                counter++;
+                streak++;
+                cnt += streak;
             } else {
-                if (counter > 0) {
-                    total += (counter * (counter + 1)) / 2; 
-                }
-                counter = 0; 
+                streak = 0;
             }
         }
-
-        if (counter > 0) {
-            total += (counter * (counter + 1)) / 2;
-        }
-
-        return total;
+        return cnt;
     }
 };
