ef741218fcc6bd22ac876403d2599ac95f3e4a60
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-08-22 11:16:50 -0400

    Time: 260 ms (53.35%), Space: 133.3 MB (97.76%) - LeetHub

--- 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -1,37 +1,22 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     int minimumArea(vector<vector<int>>& grid) {
-        int m = grid.size();
-        if (m == 0) return 0;
-        int n = grid[0].size();
+        int n = grid.size();
+        int m = grid[0].size();
+        int l = INT_MAX, u = INT_MAX;
+        int r = INT_MIN, d = INT_MIN;
 
-        bool found = false;
-        int minR = 0, minC = 0, maxR = 0, maxC = 0;
-
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
                 if (grid[i][j] == 1) {
-                    if (!found) {
-                        found = true;
-                        minR = maxR = i;
-                        minC = maxC = j;
-                    } else {
-                        minR = min(minR, i);
-                        minC = min(minC, j);
-                        maxR = max(maxR, i);
-                        maxC = max(maxC, j);
-                    }
+                    l = min(l, j);
+                    u = min(u, i);
+                    r = max(r, j);
+                    d = max(d, i);
                 }
             }
         }
 
-        if (!found) return 0; 
-        int height = maxR - minR + 1;
-        int width  = maxC - minC + 1;
-        return height * width;
+        return (r - l + 1) * (d - u + 1);
     }
 };
