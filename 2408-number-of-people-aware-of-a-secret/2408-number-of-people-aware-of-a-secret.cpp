9e56fb9d85d5b82b4a459f4fa1a3d5df99350601
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-09-09 12:13:43 -0400

    Time: 0 ms (100%), Space: 9.3 MB (84.16%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -1,37 +1,13 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int peopleAwareOfSecret(int n, int delay, int forget) {
-        const int MOD = 1'000'000'007;
-        vector<vector<int>> mat(n, vector<int>(forget, 0));
-        mat[0][0] = 1; 
-
-        auto addmod = [&](long long a, long long b){ return int((a + b) % MOD); };
-
-        for (int i = 1; i < n; ++i) {
-            long long s = 0;
-            int L = delay - 1;
-            int R = forget - 2;
-            if (L <= R) {
-                for (int j = max(0, L); j <= min(forget - 1, R); ++j) {
-                    s += mat[i - 1][j];
-                    if (s >= MOD) s -= MOD;
-                }
-            }
-            mat[i][0] = int(s % MOD);
-
-            for (int j = 1; j < forget; ++j) {
-                mat[i][j] = mat[i - 1][j - 1];
-            }
-        }
-
-        long long ans = 0;
-        for (int j = 0; j < forget; ++j) {
-            ans += mat[n - 1][j];
-            if (ans >= MOD) ans -= MOD;
-        }
-        return int(ans % MOD);
+        vector<long> dp(n + 1);
+        dp[1] = 1;
+        int share = 0, mod = 1e9 + 7, res = 0 ;
+        for (int i = 2; i <= n; ++i)
+            dp[i] = share = (share + dp[max(i - delay, 0)] - dp[max(i - forget, 0)] + mod) % mod;
+        for (int i = n - forget + 1; i <= n; ++i)
+            res = (res + dp[i]) % mod;
+        return res;
     }
 };
