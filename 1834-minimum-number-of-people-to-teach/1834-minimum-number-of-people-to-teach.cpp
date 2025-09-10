c3720f119fedcbfbf0f58dd321098bc850574c70
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-09-10 17:13:04 -0400

    Time: 10 ms (94.12%), Space: 68.3 MB (76.47%) - LeetHub

--- 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.cpp
+++ 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.cpp
@@ -1,51 +1,37 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    int minimumTeachings(int totalLanguages, vector<vector<int>>& userLanguages, vector<vector<int>>& friendships) {
-        unordered_set<int> usersToTeach;
-
-        for (auto& friendship : friendships) {
-            int user1 = friendship[0] - 1; 
-            int user2 = friendship[1] - 1;
-            bool canCommunicate = false;
-
-            for (int lang1 : userLanguages[user1]) {
-                for (int lang2 : userLanguages[user2]) {
-                    if (lang1 == lang2) {
-                        canCommunicate = true;
-                        break;
-                    }
-                }
-                if (canCommunicate) break;
-            }
-
-            if (!canCommunicate) {
-                usersToTeach.insert(user1);
-                usersToTeach.insert(user2);
+    int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
+        int m = languages.size();
+        vector<vector<char>> matrix(m + 1, vector<char>(n + 1, false));
+        for (int i = 0; i < m; i++) {
+            for (const auto& language : languages[i]) {
+                matrix[i + 1][language] = true;
             }
         }
-
-        int minUsersToTeach = userLanguages.size() + 1;
-
-        for (int language = 1; language <= totalLanguages; language++) {
-            int count = 0;
-
-            for (int user : usersToTeach) {
-                bool knowsLanguage = false;
-                for (int lang : userLanguages[user]) {
-                    if (lang == language) {
-                        knowsLanguage = true;
-                        break;
-                    }
+        vector<char> learn(m + 1, false);
+        for (const auto& friendship : friendships) {
+            bool can_communicate = false;
+            for (int i = 1; i <= n; i++) {
+                if (matrix[friendship[0]][i] && matrix[friendship[1]][i]) {
+                    can_communicate = true;
+                    break;
                 }
-                if (!knowsLanguage) count++;
             }
-
-            minUsersToTeach = min(minUsersToTeach, count);
+            if (!can_communicate) {
+                learn[friendship[0]] = true;
+                learn[friendship[1]] = true;
+            }
         }
-
-        return minUsersToTeach;
+        int minimum_learnings = INT_MAX;
+        for (int i = 1; i <= n; i++) {
+            int learnings = 0;
+            for (int j = 1; j <= m; j++) {
+                if (learn[j] && !matrix[j][i]) {
+                    learnings++;
+                }
+            }
+            minimum_learnings = min(minimum_learnings, learnings);
+        }
+        return minimum_learnings;
     }
 };
