c99eedff65a3ea50ff9d6ee040fcdef2ac559a48
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-09-11 10:34:46 -0400

    Time: 27 ms (13.45%), Space: 16.9 MB (27.48%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        vector<char> vowels;
+        unordered_map<char, int> vowels = {
+            {'A',0},{'E',0},{'I',0},{'O',0},{'U',0},
+            {'a',0},{'e',0},{'i',0},{'o',0},{'u',0}
+        };
+
         for (char c : s) {
-            if (isVowel(c)) {
-                vowels.push_back(c);
+            if (vowels.find(c) != vowels.end()) {
+                vowels[c]++;
             }
         }
 
-        sort(vowels.begin(), vowels.end());
+        vector<char> keys = {'A','E','I','O','U','a','e','i','o','u'};
+        
+        int k = 0;
 
-        int v_index = 0;
         for (int i = 0; i < s.size(); i++) {
-            if (isVowel(s[i])) {
-                s[i] = vowels[v_index++];
+            if (vowels.find(s[i]) != vowels.end()) {
+                while (vowels[keys[k]] == 0) {
+                    k++;
+                }
+                s[i] = keys[k];
+                vowels[keys[k]]--;
             }
         }
-
         return s;
-    }
-
-private:
-    bool isVowel(char c) {
-        return string("AEIOUaeiou").find(c) != string::npos;
     }
 };
