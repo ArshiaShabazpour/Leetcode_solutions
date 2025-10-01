3a61ed9cb9159ed17242a686dec3cdd57758aae8
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-09-30 23:50:10 -0400

    Time: 0 ms (100%), Space: 7.9 MB (45.09%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    int numWaterBottles(int numBottles, int numExchange) {
-        int sum =0;
-        while(numBottles/numExchange>0){
-            int temp = numBottles%numExchange;
-            sum +=numBottles -temp;
-            numBottles /= numExchange;
-            numBottles +=temp;
-        }
-        return sum+numBottles ;
+    int numWaterBottles(int b, int n) {
+        return b + (b - 1) / (n - 1);
     }
 };
