d3861541177d876582a638bcdcf5e670e93ddcc8
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-10-27 15:54:49 -0400

    Time: 11 ms (32%), Space: 27.1 MB (96.01%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int ans=0, prev=0;
-        for(auto& row: bank){
-            int device=count(row.begin(), row.end(), '1');
-            if (device>0){
-                ans+=exchange(prev,device)*device;
+        int cnt = 0;
+        int prev = 0;
+        int ans = 0;
+        for (int i = 0; i < bank.size(); i++){
+            cnt = 0;
+
+            for (char c : bank[i]){
+                if (c == '1'){
+                    cnt++;
+                }
+            }
+
+            if (cnt > 0){
+                ans += cnt * prev;
+                prev = cnt;
             }
         }
         return ans;
