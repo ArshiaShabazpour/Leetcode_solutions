4e2b6dc40bdbd26fcb2ff9349b497acdb5d6962c
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-11-13 10:21:21 -0500

    Time: 3 ms (83.33%), Space: 14.6 MB (91.3%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-    int maxOperations(string s) {
-        int ones = 0, res = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == '1')
-                ones++;
-            else if ((i > 0) && s[i - 1] == '1')
-                res += ones;
+    static int maxOperations(string& s) {
+        int cnt=0, n=s.size(), cnt1=(s[0]=='1');
+        for(int i=1; i<n; i++){
+            if (s[i]=='1') cnt1++;
+            else if (s[i-1]=='1') cnt+=cnt1;
         }
-
-        return res;
+        return cnt;
     }
 };
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
