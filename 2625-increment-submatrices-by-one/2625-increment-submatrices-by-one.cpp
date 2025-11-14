50b3c511f74a06481186c95e11dfbe843fa7b2a6
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-11-14 12:56:43 -0500

    Time: 15 ms (85.27%), Space: 86.4 MB (89.34%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>> diff(n + 1, vector<int>(n + 1, 0));
-
-        for (auto& q : queries) {
-            int r1 = q[0], c1 = q[1], r2 = q[2], c2 = q[3];
-            diff[r1][c1]++;
-            diff[r2 + 1][c1]--;
-            diff[r1][c2 + 1]--;
-            diff[r2 + 1][c2 + 1]++;
-        }
-
-        vector<vector<int>> mat(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int above = i ? mat[i - 1][j] : 0;
-                int left = j ? mat[i][j - 1] : 0;
-                int diag = (i && j) ? mat[i - 1][j - 1] : 0;
-                
-                mat[i][j] = diff[i][j] + above + left - diag;
+        vector<vector<int>>  arr(n+1, vector<int>(n, 0));
+        for(auto& q: queries){
+            const int r1=q[0], c1=q[1], r2=q[2], c2=q[3];
+            arr[r1][c1]++;
+            arr[r2+1][c1]--;
+            if (c2+1<n){
+                arr[r1][c2+1]--;
+                arr[r2+1][c2+1]++;
             }
         }
-
-        return mat;
+        for(int i=0; i<n; i++){
+            for(int j=1; j<n; j++)
+                arr[i][j]+=arr[i][j-1];
+        }
+        for(int j=0; j<n; j++){
+            for(int i=1; i<n; i++)
+                arr[i][j]+=arr[i-1][j];
+        }
+        arr.resize(n);
+        return arr;
     }
 };
