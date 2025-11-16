9427de56c2ba3989a738bed4c897d3d0b427d60e
Arshiya Shahbazpourtazehkand <155476931+ArshiaShabazpour@users.noreply.github.com>
2025-11-15 19:13:05 -0500

    Time: 159 ms (82.28%), Space: 18.1 MB (48.73%) - LeetHub

--- 3479-count-the-number-of-substrings-with-dominant-ones/3479-count-the-number-of-substrings-with-dominant-ones.cpp
+++ 3479-count-the-number-of-substrings-with-dominant-ones/3479-count-the-number-of-substrings-with-dominant-ones.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-int numberOfSubstrings(string s) {
-    int len = s.size();
-    vector<int> pref0(len);
-    vector<int> pref1(len);
-    pref0[0] = s[0] == '0';
-    pref1[0] = s[0] == '1';
-    for (int i = 1; i < len; i++) {
-        pref0[i] = pref0[i - 1] + (s[i] == '0');
-        pref1[i] = pref1[i - 1] + (s[i] == '1');
-    }
-
-
-    int count = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i; j < len;) {
-            int zeros = pref0[j] - (i ? pref0[i - 1] : 0);
-            int ones = pref1[j] - (i ? pref1[i - 1] : 0);
-
-            int skip = zeros * zeros - ones;
-
-            if (ones >= zeros * zeros) {
-                skip = max((int)sqrt(ones) - zeros, 1); // max, so it doesnt go negative
-                count += min(skip, len - j); // min, so it doenst go above remaining length
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+        vector<int> pre(n + 1);
+        pre[0] = -1;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || (i > 0 && s[i - 1] == '0')) {
+                pre[i + 1] = i;
+            } else {
+                pre[i + 1] = pre[i];
             }
-
-            j += skip; 
         }
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            int cnt0 = s[i - 1] == '0';
+            int j = i;
+            while (j > 0 && cnt0 * cnt0 <= n) {
+                int cnt1 = (i - pre[j]) - cnt0;
+                if (cnt0 * cnt0 <= cnt1) {
+                    res += min(j - pre[j], cnt1 - cnt0 * cnt0 + 1);
+                }
+                j = pre[j];
+                cnt0++;
+            }
+        }
+        return res;
     }
-    return count;
-}
 };
